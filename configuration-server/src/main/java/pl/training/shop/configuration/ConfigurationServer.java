ba8e57f8e42332ac7b57508ce50f9b34f97f0cda
landrzejewski <landrzejewski.poczta@gmail.com>
2021-11-22 15:32:34 +0100

    example

--- configuration-server/src/main/java/pl/training/shop/configuration/ConfigurationServer.java
+++ configuration-server/src/main/java/pl/training/shop/configuration/ConfigurationServer.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class ConfigurationServerApplication {
+public class ConfigurationServer {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigurationServerApplication.class, args);
+        SpringApplication.run(ConfigurationServer.class, args);
     }
 
 }
