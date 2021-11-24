828decf1fdfaba61f732d62c54e50770483e89da
landrzejewski <landrzejewski.poczta@gmail.com>
2021-11-24 11:20:14 +0100

    init

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
 ./mvnw clean package -DskipTests
 docker build -t configuration-server configuration-server
+docker build -t discovery-server discovery-server
+docker build -t gateway-server gateway-server
+docker build -t products products
+docker build -t shop shop
